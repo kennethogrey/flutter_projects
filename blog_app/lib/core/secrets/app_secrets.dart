38f8b7ab7af7d3ed4ec64e6638ad3e37cb374370
kennethogrey <ogirekenneth@gmail.com>
2025-11-11 21:56:47 +0300

    fixed security

--- blog_app/lib/core/secrets/app_secrets.dart
+++
@@ -1,4 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://qxttnassikisdkyjaski.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4dHRuYXNzaWtpc2RreWphc2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MDI3NDIsImV4cCI6MjA3NTQ3ODc0Mn0.DV781XJ5Vp2E2Yq6ykD759qzxzyTM4i9ihD5z4t_lvk';
-}
